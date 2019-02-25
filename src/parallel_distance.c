6dc4b31fc4acca821cc799567c28525a46c9b6c0
Kawin Nikomborirak <concavemail@gmail.com>
2019-02-25 17:58:01 +0000

    Add parallel_distance.c header

--- src/parallel_distance.c
+++ src/parallel_distance.c
@@ -1,10 +1,12 @@
 #include "parallel_distance.h"
 #include "math.h"
 
-double normSquared (struct Point point) { return pow(point.x, 2) + pow(point.y, 2); }
+double normSquared(struct Point point) {
+  return pow(point.x, 2) + pow(point.y, 2);
+}
 
-void distanceSquareds(struct Point points[], int num_pts, double y[]) {
+void distanceSquareds(struct Point points[], int num_pts, double distances[]) {
   for (int i = 0; i < num_pts; ++i) {
-    y[i] = normSquared(points[i]);
+    distances[i] = normSquared(points[i]);
   }
 }
--- src/parallel_distance.h
+++ src/parallel_distance.h
@@ -2,3 +2,7 @@ struct Point {
   double x;
   double y;
 };
+
+double normSquared (struct Point point);
+
+void distanceSquareds(struct Point points[], int num_pts, double distances[]);

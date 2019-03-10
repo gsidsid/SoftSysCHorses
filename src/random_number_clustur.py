b3f4d819e501ac54d96df163d6fc6c8436e1e49c
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-03-10 18:19:43 -0400

    generates random numbers in clusters with radius r
    For checking our k means algorithm

---
+++ src/random_number_clustur.py
@@ -0,0 +1,9 @@
+def rand_clusters(k,n,r, a,b,c,d):
+    """return k clusters of n points each in random disks of radius r
+where the centers of the disk are chosen randomly in [a,b]x[c,d]"""
+    clusters = []
+    for _ in range(k):
+        x = a + (b-a)*random()
+        y = c + (d-c)*random()
+        clusters.extend(rand_cluster(n,(x,y),r))
+    return clusters

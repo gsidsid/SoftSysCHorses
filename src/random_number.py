0b1ebd1e2c764c07b87fd437fb19a6e3b7a6bacb
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-03-21 09:11:59 -0400

    csv numbers

--- src/random_number.py
+++ src/random_number.py
@@ -1,5 +1,5 @@
 import random
-
+import csv
 radius = 200
 rangeX = (0, 2500)
 rangeY = (0, 2500)
@@ -11,14 +11,25 @@ for x in range(-radius, radius+1):
             deltas.add((x,y))
 
 randPoints = []
-excluded = set()# excludes all points within radius of previous points
+#excluded = set()# excludes all points within radius of previous points
 #to increase randomness
 i = 0
 while i<qty:
     x = random.randrange(*rangeX)
     y = random.randrange(*rangeY)
-    if (x,y) in excluded: continue
+    #if (x,y) in excluded: continue
     randPoints.append((x,y))
     i += 1
-    excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
-print randPoints
+
+    #excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
+
+print (randPoints)
+
+import csv
+
+item_length = len(randPoints[0])
+
+with open('test.csv', 'wb') as test_file:
+  file_writer = csv.writer(test_file)
+  for i in range(item_length):
+    file_writer.writerow([x[i] for x in randPoints])
--- src/random_number_clustur.py
+++ src/random_number_clustur.py
@@ -1,3 +1,4 @@
+import random
 def rand_clusters(k,n,r, a,b,c,d):
     """return k clusters of n points each in random disks of radius r
 where the centers of the disk are chosen randomly in [a,b]x[c,d]"""
@@ -7,3 +8,4 @@ where the centers of the disk are chosen randomly in [a,b]x[c,d]"""
         y = c + (d-c)*random()
         clusters.extend(rand_cluster(n,(x,y),r))
     return clusters
+rand_clusters(3,1000,100,1,1,1,1);

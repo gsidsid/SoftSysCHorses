5e5e7c8ad22ba572732d5bd9f83ca24f7d11e18d
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-03-21 10:17:46 -0400

    simpler random generarion

--- src/random_number.py
+++ src/random_number.py
@@ -2,29 +2,29 @@ import random
 import csv
 from numpy import vstack,array
 from numpy.random import rand
-radius = 200
-rangeX = (0, 2500)
-rangeY = (0, 2500)
-qty = 1000
-deltas = set()
-for x in range(-radius, radius+1):
-    for y in range(-radius, radius+1):
-        if x*x + y*y <= radius*radius:
-            deltas.add((x,y))
-
-randPoints = []
-#excluded = set()# excludes all points within radius of previous points
-#to increase randomness
-i = 0
-while i<qty:
-    x = random.randrange(*rangeX)
-    y = random.randrange(*rangeY)
-    #if (x,y) in excluded: continue
-    randPoints.append((x,y))
-    i += 1
-
-    #excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
-randPoints = vstack((rand(150,2) + array([.5,.5]),rand(150,2)))
+# radius = 200
+# rangeX = (0, 2500)
+# rangeY = (0, 2500)
+# qty = 1000
+# deltas = set()
+# for x in range(-radius, radius+1):
+#     for y in range(-radius, radius+1):
+#         if x*x + y*y <= radius*radius:
+#             deltas.add((x,y))
+#
+# randPoints = []
+# #excluded = set()# excludes all points within radius of previous points
+# #to increase randomness
+# i = 0
+# while i<qty:
+#     x = random.randrange(*rangeX)
+#     y = random.randrange(*rangeY)
+#     #if (x,y) in excluded: continue
+#     randPoints.append((x,y))
+#     i += 1
+#
+#     #excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
+randPoints = vstack((rand(150,2) *2500 ,rand(150,2)*2500))
 
 print (randPoints)
 

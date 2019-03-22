02e362294e881bb7eb104bc8baf4d75416b640a6
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-03-21 20:10:47 -0400

    float vs int

--- src/random_number.py
+++ src/random_number.py
@@ -3,33 +3,34 @@ import csv
 from numpy import vstack,array
 from numpy.random import rand
 import numpy
-# radius = 200
-# rangeX = (0, 2500)
-# rangeY = (0, 2500)
-# qty = 1000
-# deltas = set()
-# for x in range(-radius, radius+1):
-#     for y in range(-radius, radius+1):
-#         if x*x + y*y <= radius*radius:
-#             deltas.add((x,y))
-#
-# randPoints = []
-# #excluded = set()# excludes all points within radius of previous points
-# #to increase randomness
-# i = 0
-# while i<qty:
-#     x = random.randrange(*rangeX)
-#     y = random.randrange(*rangeY)
-#     #if (x,y) in excluded: continue
-#     randPoints.append((x,y))
-#     i += 1
-#
-#     #excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
-randPoints = vstack((rand(150,2) *2500 ,rand(150,2)*2500))
+radius = 200
+rangeX = (0, 2500)
+rangeY = (0, 2500)
+qty = 1000
+deltas = set()
+for x in range(-radius, radius+1):
+    for y in range(-radius, radius+1):
+        if x*x + y*y <= radius*radius:
+            deltas.add((x,y))
+
+randPoints = []
+#excluded = set()# excludes all points within radius of previous points
+#to increase randomness
+i = 0
+while i<qty:
+    x = random.randrange(*rangeX)
+    y = random.randrange(*rangeY)
+    #if (x,y) in excluded: continue
+    randPoints.append((x,y))
+    i += 1
+
+    #excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
+randPoints_float = vstack((rand(150,2) *2500 ,rand(150,2)*2500))
 
 print (randPoints)
-numpy.savetxt("random.csv", randPoints, delimiter=",")
+numpy.savetxt("random_float.csv", randPoints_float, delimiter=",")
 
+numpy.savetxt("random_int.csv", randPoints, delimiter=",")
 # import csv
 #
 # item_length = len(randPoints[0])

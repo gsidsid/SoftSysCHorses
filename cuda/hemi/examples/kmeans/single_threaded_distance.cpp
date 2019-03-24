716a75a12d65ecfea1c22a669246d1965efe9a4d
Siddharth Garimella <gsidsid@gmail.com>
2019-03-24 14:39:23 -0400

    need to rethink host vs device function usage

--- cuda/hemi/examples/kmeans/single_threaded_distance.cpp
+++ cuda/hemi/examples/kmeans/single_threaded_distance.cpp
@@ -5,7 +5,7 @@
 
 using namespace hemi;
 
-__host__ __device__ void distanceSquareds(const Point origin, const Point *points,
+void distanceSquareds(const Point origin, const Point *points,
                                           const int num_pts, double *distances) {
     parallel_for(0, num_pts, [distances,origin,points] HEMI_LAMBDA (int i) {
         distances[i] = distanceSquared(origin, points[i]);
